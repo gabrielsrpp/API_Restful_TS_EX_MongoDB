10457eaa2a367d8932bb11f684b6cbc2c3b3e445
gabrielsrpp <gabrielpires2003@hotmail.com>
2025-07-02 11:01:27 -0300

    Integrando Express

---
+++ config/default.ts
@@ -0,0 +1,3 @@
+export default {
+    portt: 3000
+};
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,16 @@
-console.log("Deu boa !");
+import express from "express"
+import config from "config"
+
+const app = express()
+
+//Json middleware
+
+app.use(express.json())
+
+//app port 
+const port = config.get<number>("port");
+
+app.listen(port, async () => {
+    console.log("Aplicação esta funcionando na porta : ${port");
+});
+
