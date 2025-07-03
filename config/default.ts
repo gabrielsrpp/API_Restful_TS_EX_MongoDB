341cdf92ab2b1d2f4c3e07337b9f3973c7dfaa8a
gabrielsrpp <gabrielpires2003@hotmail.com>
2025-07-03 09:53:44 -0300

    Conectando ao banco de dados

--- config/default.ts
+++ config/default.ts
@@ -1,3 +1,4 @@
 export default {
     port: 3000,
+    dbUri: "mongodb+srv://test:123@cluster0.e1kle1s.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 };
--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,9 @@ const app = express()
 //Json middleware
 app.use(express.json())
 
+// DB
+import db from "../config/db"
+
 // routers
 import router from "./router"
 
@@ -15,6 +18,7 @@ app.use("/api/", router);
 const port = config.get<number>("port");
 
 app.listen(port, async () => {
-    console.log("Aplicação esta funcionando na porta : ${port");
+    await db();
+    console.log(`Aplicação esta funcionando na porta : ${port}`);
 });
 
