ed22b0927574a8431f26929b9320f050d3ac2750
gabrielsrpp <gabrielpires2003@hotmail.com>
2025-07-03 11:32:25 -0300

    Inserindo dados

--- src/controllers/movieController.ts
+++ src/controllers/movieController.ts
@@ -8,5 +8,11 @@ import { MovieModel } from "../models/Movie";
 import Logger from "../../config/logger";
 
 export async function createMovie(req: Request, res: Response) {
-    res.status(200).send("Deu certo o controller !");
+    try {
+        const data = req.body
+        const movie = await MovieModel.create(data)
+        res.status(201).json(movie)
+    } catch (e: any) {
+        Logger.error(`Erro no sistema: ${e.message}`);
+    }
 }
